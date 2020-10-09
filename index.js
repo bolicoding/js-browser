8b08c2560e1b899873abb6bc267c54641e802970
bolicoding <bolicoding@gmail.com>
2020-10-09 13:42:49 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+fullName = (first, last) => {
+  return `${first} ${last}`
+}
+
+alert(fullName('Boli', 'Mon'))
+
+console.log('');
+
+
+// DOM --> Document Object Model
+
+const user = {
+  name: 'Ashley',
+  email: 'ash@gmail.com',
+  favs: [
+    'Chipotle',
+    'Chic fil a'
+  ]
+}
+
